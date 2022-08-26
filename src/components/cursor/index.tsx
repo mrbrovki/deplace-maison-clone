97c02d5be052559f8afcec43a0715afae9bc4f45
Mahkamjon Raupov <devbrovki@gmail.com>
2022-08-27 01:58:30 +0500

    gooey cursor implemented

---
+++ src/components/cursor/index.tsx
@@ -0,0 +1,61 @@
+import { FC, ReactElement, useCallback, useEffect, useState } from 'react';
+import styled from 'styled-components';
+
+interface Coords{
+  x: number;
+  y: number;
+};
+
+interface DotProps{
+  coords: Coords;
+  id: number;
+};
+
+let bigDot = 40;
+let smallDot = 5;
+let amount = 40;
+let xDot = (bigDot - smallDot) / amount;
+let duration = 0.004;
+const dots:ReactElement<DotProps>[] = [];
+
+const Dot = styled.span.attrs((props:DotProps) => ({style:{
+  top: props.coords.y - (bigDot - (props.id * xDot)) / 2,
+  left: props.coords.x - (bigDot - (props.id * xDot)) / 2,
+  }}))`
+  position: fixed;
+  transition: all ${(props: DotProps) => props.id * duration}s ease-out;
+  width: ${(props: DotProps) => bigDot - (props.id * xDot)}px;
+  height: ${(props: DotProps) => bigDot - (props.id * xDot)}px;
+  border-radius: 20px;
+  background-color: white;
+`;
+
+const StyledCursor = styled.span`
+  mix-blend-mode: difference;
+  position: fixed;
+  z-index: 1000;
+`;
+
+const Cursor:FC = () => {
+  const [coords, setCoords] = useState<Coords>({x: 0, y: 0});
+  
+  const mouseMoveEvent = useCallback((e:MouseEvent) => {
+    setCoords({x: e.clientX, y: e.clientY});
+  }, []);
+  
+  useEffect(()=>{
+    document.addEventListener('mousemove', mouseMoveEvent);
+  }, []);
+  
+  for(let i = 0; i < amount; i++){
+    dots.push(<Dot coords={coords} id={i} key={i}/>);
+  }
+  
+  return (
+    <StyledCursor>
+      {dots}
+    </StyledCursor>
+  )
+}
+
+export default Cursor;
