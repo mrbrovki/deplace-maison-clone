07f9233a13a93b9eb66c9e4e49816f837c91a8af
Mahkamjon Raupov <devbrovki@gmail.com>
2022-08-29 02:48:59 +0500

    global styles added

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+import {createGlobalStyle} from 'styled-components';
+import Transition from './components/transition';
+import Cursor from './components/cursor';
+import AppContext from './context';
+import Pages from './pages';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+const GlobalStyle = createGlobalStyle`
+  body{
+    box-sizing: border-box;
+    margin: 0;
+    background-color: #fff;
+  }
+`;
+
 root.render(
   <React.StrictMode>
-    <App />
+    <AppContext>
+      <GlobalStyle />
+      <Cursor />
+      <Transition />
+      <BrowserRouter>
+        <Pages />
+      </BrowserRouter>
+    </AppContext>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
